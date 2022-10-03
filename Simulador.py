395d99f14db84a4a27ff101779ac94d1f6cb6b26
DRogerSC <musicfan211@hotmail.com>
2022-10-03 10:31:44 -0500

    Initial commit, first script with initial template and layout

---
+++ Simulador.py
@@ -0,0 +1,30 @@
+import matplotlib
+import numpy as np
+import pandas as pd
+
+
+class simulador:
+    private:
+
+    public:
+
+
+def main (argv, argc):
+        
+
+    #Primer entrada:
+
+        #Numero de parametros, step time
+    
+    #Segunda entrada: 
+
+        #Iteracion de parametros A & B
+
+    #Tercera entrada
+
+        #Tipo de senal: Escalon, senoidal, leer de .txt
+
+    #Cuarta entrada
+
+        #Numero de pasos a simular, opcion indefinida para simulacion a tiempo real
+
